fc88d63c88b83a5246a7ff9b8dac075be19a6001
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2025-06-03 14:47:03 +0100

    Cornell box now has a single round light with 2 colors.

--- scenes/cornell_box.glsl
+++ scenes/cornell_box.glsl
@@ -5,29 +5,15 @@
 const float wall_pos = 4.0;
 const float wall_height = 6.0;
 
-const vec3 lightSize = vec3(1.0, 0.0, 2.0);
-const vec3 lightOffset = vec3(3.0, epsilon, wall_pos - lightSize.z / 2.0);
+const vec3 lightSize = vec3(1.0, 0.0, 1.0) * 4.0;
+const vec3 lightOffset = vec3(wall_pos, epsilon, wall_pos) - lightSize / 2.0;
 
-const Quad worldLights[] = Quad[](
-	Quad(
-		vec3(-wall_pos + lightOffset.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z + lightSize.z),
-		vec3(-wall_pos + lightOffset.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z),
-		vec3(-wall_pos + lightOffset.x + lightSize.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z),
-		vec3(
-			-wall_pos + lightOffset.x + lightSize.x,
-			wall_height - lightOffset.y,
-			-wall_pos + lightOffset.z + lightSize.z
-		)
-	),
-	Quad(
-		vec3(wall_pos - lightOffset.x - lightSize.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z),
-		vec3(wall_pos - lightOffset.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z),
-		vec3(wall_pos - lightOffset.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z + lightSize.z),
-		vec3(
-			wall_pos - lightOffset.x - lightSize.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z + lightSize.z
-		)
-	)
-);
+const Quad worldLights[] = Quad[](Quad(
+	vec3(-wall_pos + lightOffset.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z + lightSize.z),
+	vec3(-wall_pos + lightOffset.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z),
+	vec3(-wall_pos + lightOffset.x + lightSize.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z),
+	vec3(-wall_pos + lightOffset.x + lightSize.x, wall_height - lightOffset.y, -wall_pos + lightOffset.z + lightSize.z)
+));
 
 bool hit_world(Ray r, float tmin, float tmax, inout HitRecord rec) {
 	bool hit = false;
@@ -121,13 +107,21 @@ bool hit_world(Ray r, float tmin, float tmax, inout HitRecord rec) {
 	// Lights
 	if (hit_quad(worldLights[0], r, tmin, rec.t, rec)) {
 		hit = true;
-		rec.material = createDiffuseMaterial(vec3(0.0));
-		rec.material.emissive = vec3(1.0f, 1.0f, 0.0f) * 3.0;
-	}
-	if (hit_quad(worldLights[1], r, tmin, rec.t, rec)) {
-		hit = true;
-		rec.material = createDiffuseMaterial(vec3(0.0));
-		rec.material.emissive = vec3(1.0f, 0.0f, 1.0f) * 3.0;
+		rec.material = createDiffuseMaterial(vec3(1.0));
+
+		float u = rec.coord.x;
+		float v = rec.coord.y;
+		float u2 = 4.0 * (u - 0.5) * (u - 0.5);
+		float v2 = 4.0 * (v - 0.5) * (v - 0.5);
+		float angle = atan(v - 0.5, u - 0.5);
+		float len = length(vec2(u - 0.5, v - 0.5)) * 2.0;
+		if (len < 1.0) {
+			if (angle < 0.0) {
+				rec.material.emissive = vec3(1.0, 1.0, 0.0) * 5.0;
+			} else {
+				rec.material.emissive = vec3(0.0, 0.0, 1.0) * 5.0;
+			}
+		}
 	}
 
 	if (hit_sphere(Sphere(vec3(-2.0, 1.5, -1.5), 1.5), r, tmin, rec.t, rec)) {
